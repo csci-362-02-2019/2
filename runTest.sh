ab5e7f551c8dcb583ab2784abc774d689f012e9b
ledbetterw <54377100+ledbetterw@users.noreply.github.com>
2019-10-28 21:09:26 -0400

    Add files via upload

--- runTest.sh
+++ runTest.sh
@@ -4,7 +4,7 @@ echo "changing directory to testCases."
 cd /home/wright/testCases
 echo "read test cases"
 
-#rm -r ../reports/* #clear the reports directory.
+rm -r /home/wright/testCases/reports/* #clear the reports directory.
 
 echo "<!DOCTYPE html>" >> /home/wright/testCases/reports/testReport.html
 echo "<html>" >> /home/wright/testCases/reports/testReport.html
@@ -22,5 +22,71 @@ echo "<th> input </th>" >> /home/wright/testCases/reports/testReport.html
 echo "<th> expected output </th>" >> /home/wright/testCases/reports/testReport.html
 echo "<th> actual output </th>" >> /home/wright/testCases/reports/testReport.html
 echo "<th> pass/fail </th></tr>" >> /home/wright/testCases/reports/testReport.html
+
+testCaseTracker=0 #tracks the number of the test case being worked on.
+lineStorage=( ) #array for storing lines from testCase files
+
+for filename in * 
+do
+echo "<tr>" >> /home/wright/testCases/reports/testReport.html
+
+	counter=0
+		while IFS='' read -r line;do #reads lines from a test Case file
+			lineStorage[$counter]="$line"
+# 0=testCaseNumber, 1=Requirement, 2=ClassName, 3=MethodName, 4=input, 5=expectedOutput, 6=classpath, # 7=DriverName
+
+		if [ "$counter" -eq 0 ] || [ "$counter" -eq 1 ] || [ "$counter" -eq 2 ] || [ "$counter" -eq 4 ]
+		 then 
+			echo "<td>" >> /home/wright/testCases/reports/testReport.html
+			echo ${lineStorage[$counter]} >> /home/wright/testCases/reports/testReport.html
+
+	
+elif [ "$counter" -eq 3 ];then
+        	        echo "<td>" >> /home/wright/testCases/reports/testReport.html
+              
+        	       METHOD=${lineStorage[$counter]}
+        	       MyMethod=${METHOD##* }
+        	        echo $METHOD | sed 's/,.*//' >> /home/wright/testCases/reports/testReport.html
+		
+		  fi
+
+
+
+	((counter++))
+	        echo "</td>" >> /home/wright/testCases/reports/testReport.html
+	done < "$filename"
+
+cd ..
+
+echo "<td>" >> /home/wright/testCases/reports/testReport.html
+echo ${lineStorage[5]}>> /home/wright/testCases/reports/testReport.html
+echo "</td>" >> /home/wright/testCases/reports/testReport.html 
+
+output=$(echo ${lineStorage[4]} | java -classpath ${lineStorage[6]} "${lineStorage[4]}")
+echo "<td>" >> /home/wright/testCases/reports/testReport.html
+echo $output >> /home/wright/testCases/reports/testReport.html
+echo "</td>" >> /home/wright/testCases/reports/testReport.html
+
+echo "<td>" >> /home/wright/testCases/reports/testReport.html
+if [ "$output" == "${lineStorage[5]}" ];then	
+	echo "<p style="color:green"> Passed.</p>" >> /home/wright/testCases/reports/testReport.html 
+
+else
+	echo "<p style="color:red"> Failed.</p>" >> /home/wright/testCases/reports/testReport.html 
+fi
+echo "</td>" >> /home/wright/testCases/reports/testReport.html
+
+
+
+
+echo "</tr>" >> /home/wright/testCases/reports/testReport.html #ends a row in the table
+echo "test case $testCaseTracker complete"
+((testCaseTracker++))
+cd testCases
+done
+
+cd /home/wright/testCases/reports
+
+xdg-open testReport.html
 
 
