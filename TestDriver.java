8d23ac2af44932ee6e59e30246cbb98e68f2894f
samlemonsam <42654999+samlemonsam@users.noreply.github.com>
2019-10-28 20:11:31 -0400

    Update TestDriver.java

--- TestDriver.java
+++ TestDriver.java
@@ -2,6 +2,9 @@ package org.openmrs;
 
 import org.openmrs.patient.impl.LuhnIdentifierValidator;
 import org.openmrs.patient.UnallowedIdentifierException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 //these are what return true for isValid() method
 //a-3
@@ -19,37 +22,49 @@ public class TestDriver extends LuhnIdentifierValidator {
 	
 	public static void main(String[] args) {
 		
-		//Test 1
-		LuhnIdentifierValidator LIV = new LuhnIdentifierValidator();
-		boolean result1;
-		result1 = LIV.isValid("a-3");
-		System.out.println(result1);
-		
-		//Test 2
-		boolean result2;
-		result2 = LIV.isValid("a-4");
-		System.out.println(result2);
-		
-		//Test 3
 		try {
-			boolean result3;
-			result3 = LIV.isValid("Sam");
-			System.out.println(result3);
-		}
-		catch (UnallowedIdentifierException e) {
-			System.out.println("UnallowedIdentifierException thrown.");
-		}
-		//Test 4
-		LuhnIdentifierValidator LIV2 = new LuhnIdentifierValidator();
-		int result4;
-		result4 = LIV2.getCheckDigit("a");
-		System.out.println(result4);
-		
-		//Test 5
-		int result5;
-		result5 = LIV2.getCheckDigit("b");
-		System.out.println(result5);
+			//Test 1
+			LuhnIdentifierValidator LIV0 = new LuhnIdentifierValidator();
+			boolean result1;
+			String line1 = Files.readAllLines(Paths.get("/Users/samle/OneDrive/Documents/CSCI 362/testcase1.txt")).get(4);
+			result1 = LIV0.isValid(line1);
+			System.out.println(result1);
+			
+			//Test 2
+			boolean result2;
+			String line2 = Files.readAllLines(Paths.get("/Users/samle/OneDrive/Documents/CSCI 362/testcase2.txt")).get(4);
+			result2 = LIV0.isValid(line2);
+			System.out.println(result2);
 
 		
+			//Test 3
+			try {
+				LuhnIdentifierValidator LIV1 = new LuhnIdentifierValidator();
+				boolean result3;
+				String line3 = Files.readAllLines(Paths.get("/Users/samle/OneDrive/Documents/CSCI 362/testcase3.txt")).get(4);
+				result3 = LIV1.isValid(line3);
+				System.out.println(result3);
+			}
+			catch (UnallowedIdentifierException | IOException e) {
+				System.out.println("UnallowedIdentifierException thrown.");
+			}
+		
+			//Test 4
+			LuhnIdentifierValidator LIV2 = new LuhnIdentifierValidator();
+			int result4;
+			String line4 = Files.readAllLines(Paths.get("/Users/samle/OneDrive/Documents/CSCI 362/testcase4.txt")).get(4);
+			result4 = LIV2.getCheckDigit(line4);
+			System.out.println(result4);
+
+			//Test 5
+			LuhnIdentifierValidator LIV3 = new LuhnIdentifierValidator();
+			int result5;
+			String line5 = Files.readAllLines(Paths.get("/Users/samle/OneDrive/Documents/CSCI 362/testcase5.txt")).get(4);
+			result5 = LIV3.getCheckDigit(line5);
+			System.out.println(result5);
+		}
+		catch (IOException e) {
+			System.out.println("error :(");
+		}
 	}
 }
