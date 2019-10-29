c32de492f4672e54eb75487925623e3f6b3c2a56
ledbetterw <54377100+ledbetterw@users.noreply.github.com>
2019-10-29 11:11:10 -0400

    Add files via upload

--- runTest.sh
+++ runTest.sh
@@ -12,39 +12,39 @@ echo "<head>" >> /home/wright/testCases/reports/testReport.html
 echo "<title> Test Report</title>" >> /home/wright/testCases/reports/testReport.html
 echo "</head>" >> /home/wright/testCases/reports/testReport.html
 echo "<body>" >> /home/wright/testCases/reports/testReport.html
-echo "<table border=1 style=width:75%>" >> /home/wright/testCases/reports/testReport.html
+echo "<table border=1 style=width:10%>" >> /home/wright/testCases/reports/testReport.html
 echo "<tr>" >> /home/wright/testCases/reports/testReport.html
 echo "<th> Test # </th>" >> /home/wright/testCases/reports/testReport.html
 echo "<th> Requirement </th>" >> /home/wright/testCases/reports/testReport.html
 echo "<th> class name </th>" >> /home/wright/testCases/reports/testReport.html
 echo "<th> method name </th>" >> /home/wright/testCases/reports/testReport.html
 echo "<th> input </th>" >> /home/wright/testCases/reports/testReport.html
-echo "<th> expected output </th>" >> /home/wright/testCases/reports/testReport.html
+echo "<th> oracle </th>" >> /home/wright/testCases/reports/testReport.html
 echo "<th> actual output </th>" >> /home/wright/testCases/reports/testReport.html
 echo "<th> pass/fail </th></tr>" >> /home/wright/testCases/reports/testReport.html
 
-testCaseTracker=0 #tracks the number of the test case being worked on.
-lineStorage=( ) #array for storing lines from testCase files
+testNum=1 #tracks the number of the test case being worked on.
+lineNum=( ) #array for storing lines from testCase files
 
 for filename in * 
 do
 echo "<tr>" >> /home/wright/testCases/reports/testReport.html
 
-	counter=0
-		while IFS='' read -r line;do #reads lines from a test Case file
-			lineStorage[$counter]="$line"
+	count=0
+		while IFS='' read line;do #reads lines from a test Case file
+			lineNum[$count]="$line"
 # 0=testCaseNumber, 1=Requirement, 2=ClassName, 3=MethodName, 4=input, 5=expectedOutput, 6=classpath, # 7=DriverName
 
-		if [ "$counter" -eq 0 ] || [ "$counter" -eq 1 ] || [ "$counter" -eq 2 ] || [ "$counter" -eq 4 ]
+		if [ "$count" -eq 0 ] || [ "$count" -eq 1 ] || [ "$count" -eq 2 ] || [ "$count" -eq 4 ]
 		 then 
 			echo "<td>" >> /home/wright/testCases/reports/testReport.html
-			echo ${lineStorage[$counter]} >> /home/wright/testCases/reports/testReport.html
+			echo ${lineNum[$count]} >> /home/wright/testCases/reports/testReport.html
 
 	
-elif [ "$counter" -eq 3 ];then
+elif [ "$count" -eq 3 ];then
         	        echo "<td>" >> /home/wright/testCases/reports/testReport.html
               
-        	       METHOD=${lineStorage[$counter]}
+        	       METHOD=${lineNum[$count]}
         	       MyMethod=${METHOD##* }
         	        echo $METHOD | sed 's/,.*//' >> /home/wright/testCases/reports/testReport.html
 		
@@ -52,7 +52,7 @@ elif [ "$counter" -eq 3 ];then
 
 
 
-	((counter++))
+	((count++))
 	        echo "</td>" >> /home/wright/testCases/reports/testReport.html
 	done < "$filename"
 
@@ -61,18 +61,18 @@ cd ..
 
 #prints out expected output from test case
 echo "<td>" >> /home/wright/testCases/reports/testReport.html
-echo ${lineStorage[5]}>> /home/wright/testCases/reports/testReport.html
+echo ${lineNum[5]}>> /home/wright/testCases/reports/testReport.html
 echo "</td>" >> /home/wright/testCases/reports/testReport.html 
 
 #pipes input into java driver classes and puts them in html table
-output=$(echo ${lineStorage[4]} | java -classpath ${lineStorage[6]} ${lineStorage[7]} "${lineStorage[4]}")
+output=$(echo ${lineNum[5]} | java -cp "${lineNum[6]}" ${lineNum[7]} "${lineNum[4]}")
 echo "<td>" >> /home/wright/testCases/reports/testReport.html
 echo $output >> /home/wright/testCases/reports/testReport.html
 echo "</td>" >> /home/wright/testCases/reports/testReport.html
 
 #compares expected output to actual output and decides if test passed or failed
 echo "<td>" >> /home/wright/testCases/reports/testReport.html
-if [ "$output" == "${lineStorage[5]}" ];then	
+if [ "$output" == "${lineNum[5]}" ];then	
 	echo "<p style="color:green"> Passed.</p>" >> /home/wright/testCases/reports/testReport.html 
 
 else
@@ -84,8 +84,8 @@ echo "</td>" >> /home/wright/testCases/reports/testReport.html
 
 
 echo "</tr>" >> /home/wright/testCases/reports/testReport.html #ends a row in the table
-echo "test case $testCaseTracker complete"
-((testCaseTracker++))
+echo "test case $testNum complete"
+((testNum++))
 cd testCases
 done
 
