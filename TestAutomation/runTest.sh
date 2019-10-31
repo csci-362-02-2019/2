eafed251f043b219b2d191ddd09243c812462296
Wright <ledbetterw@g.cofc.edu>
2019-10-31 11:50:05 -0400

    Updated directories in runTest.sh

--- TestAutomation/runTest.sh
+++ TestAutomation/runTest.sh
@@ -1,96 +1,97 @@
 #!/bin/bash
 
-echo "changing directory to testCases."
-cd /home/wright/testCases
-echo "read test cases"
+echo "changing directory to testCases..."
+cd testCases
+echo "reading test cases..."
 
-rm -r /home/wright/testCases/reports/* #clear the reports directory.
+rm -r ../reports/*	#clear the reports directory
+#sets up initial html Doc---------------------------------------------------
+echo "<!DOCTYPE html>" >>../reports/testReport.html
+echo  "<html>" >> ../reports/testReport.html
+echo "<head>" >> ../reports/testReport.html
+echo  "<title>Test Report</title>" >> ../reports/testReport.html
+echo  "</head>" >> ../reports/testReport.html
+echo  "<body>" >> ../reports/testReport.html
+echo  "<table border=1 style=width:75%>" >> ../reports/testReport.html
+echo  "<tr>" >> ../reports/testReport.html
+echo  "<th> Test # </th>">> ../reports/testReport.html
+echo  "<th> Requirement </th>" >> ../reports/testReport.html
+echo  "<th> class name </th>">> ../reports/testReport.html
+echo  "<th> method name </th>">> ../reports/testReport.html
+echo  "<th> input </th>">> ../reports/testReport.html
+echo  "<th> expected output </th>">> ../reports/testReport.html
+echo  "<th> actual output </th>" >> ../reports/testReport.html
+echo  "<th> pass/fail </th></tr>">> ../reports/testReport.html
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 
-echo "<!DOCTYPE html>" >> /home/wright/testCases/reports/testReport.html
-echo "<html>" >> /home/wright/testCases/reports/testReport.html
-echo "<head>" >> /home/wright/testCases/reports/testReport.html
-echo "<title> Test Report</title>" >> /home/wright/testCases/reports/testReport.html
-echo "</head>" >> /home/wright/testCases/reports/testReport.html
-echo "<body>" >> /home/wright/testCases/reports/testReport.html
-echo "<table border=1 style=width:10%>" >> /home/wright/testCases/reports/testReport.html
-echo "<tr>" >> /home/wright/testCases/reports/testReport.html
-echo "<th> Test # </th>" >> /home/wright/testCases/reports/testReport.html
-echo "<th> Requirement </th>" >> /home/wright/testCases/reports/testReport.html
-echo "<th> class name </th>" >> /home/wright/testCases/reports/testReport.html
-echo "<th> method name </th>" >> /home/wright/testCases/reports/testReport.html
-echo "<th> input </th>" >> /home/wright/testCases/reports/testReport.html
-echo "<th> oracle </th>" >> /home/wright/testCases/reports/testReport.html
-echo "<th> actual output </th>" >> /home/wright/testCases/reports/testReport.html
-echo "<th> pass/fail </th></tr>" >> /home/wright/testCases/reports/testReport.html
 
-testNum=1 #tracks the number of the test case being worked on.
-lineNum=( ) #array for storing lines from testCase files
 
-for filename in * 
+testCaseTracker=1  #tracks what test case is being worked on
+lineStorage=( ) #array for storing lines from test case files
+for filename in *    #goes through all the files in testCase dir
 do
-echo "<tr>" >> /home/wright/testCases/reports/testReport.html
-
-	count=0
-		while IFS='' read line;do #reads lines from a test Case file
-			lineNum[$count]="$line"
-# 0=testCaseNumber, 1=Requirement, 2=ClassName, 3=MethodName, 4=input, 5=expectedOutput, 6=classpath, # 7=DriverName
-
-		if [ "$count" -eq 0 ] || [ "$count" -eq 1 ] || [ "$count" -eq 2 ] || [ "$count" -eq 4 ]
-		 then 
-			echo "<td>" >> /home/wright/testCases/reports/testReport.html
-			echo ${lineNum[$count]} >> /home/wright/testCases/reports/testReport.html
-
-	
-elif [ "$count" -eq 3 ];then
-        	        echo "<td>" >> /home/wright/testCases/reports/testReport.html
+echo "<tr>" >> ../reports/testReport.html
+   
+   counter=0
+	while IFS='' read -r line;do #reads lines from a test Case file
+	   lineStorage[$counter]="$line" 
+#0=TestCaseNumber, 1=Requirement, 2=ClassName, 3=MethodName, 4=input, 5=expectedOutput, 6=classpath, 7=DriverName
+	  if [ "$counter" -eq 0 ] || [ "$counter" -eq 1 ] || [ "$counter" -eq 2 ] || [ "$counter" -eq 4 ] 
+	   then
+		echo "<td>" >> ../reports/testReport.html
+		echo ${lineStorage[$counter]} >> ../reports/testReport.html
+           elif [ "$counter" -eq 3 ];then
+                echo "<td>" >> ../reports/testReport.html
               
-        	       METHOD=${lineNum[$count]}
-        	       MyMethod=${METHOD##* }
-        	        echo $METHOD | sed 's/,.*//' >> /home/wright/testCases/reports/testReport.html
+               METHOD=${lineStorage[$counter]}
+               MyMethod=${METHOD##* }
+                echo $METHOD | sed 's/,.*//' >> ../reports/testReport.html
 		
-		  fi
+	  fi
 
 
 
-	((count++))
-	        echo "</td>" >> /home/wright/testCases/reports/testReport.html
+
+
+
+	((counter++))
+	        echo "</td>" >> ../reports/testReport.html
 	done < "$filename"
 
-cd ..
 
+cd .. #go back to TestAutomation directory
 
 #prints out expected output from test case
-echo "<td>" >> /home/wright/testCases/reports/testReport.html
-echo ${lineNum[5]}>> /home/wright/testCases/reports/testReport.html
-echo "</td>" >> /home/wright/testCases/reports/testReport.html 
+echo "<td>" >> reports/testReport.html
+echo ${lineStorage[5]}>> reports/testReport.html
+echo "</td>" >> reports/testReport.html
 
 #pipes input into java driver classes and puts them in html table
-output=$(echo ${lineNum[5]} | java -cp "${lineNum[6]}" ${lineNum[7]} "${lineNum[4]}")
-echo "<td>" >> /home/wright/testCases/reports/testReport.html
-echo $output >> /home/wright/testCases/reports/testReport.html
-echo "</td>" >> /home/wright/testCases/reports/testReport.html
+output=$(echo ${lineStorage[4]} | java -classpath ${lineStorage[6]} ${lineStorage[7]} "${lineStorage[4]}")
+echo "<td>" >> reports/testReport.html
+echo $output >> reports/testReport.html
+echo "</td>" >> reports/testReport.html
+
 
 #compares expected output to actual output and decides if test passed or failed
-echo "<td>" >> /home/wright/testCases/reports/testReport.html
-if [ "$output" == "${lineNum[5]}" ];then	
-	echo "<p style="color:green"> Passed.</p>" >> /home/wright/testCases/reports/testReport.html 
-
+echo "<td" >> reports/testReport.html
+if [ "$output" == "${lineStorage[5]}" ];then
+   echo "style=\"color:#0D7010;\">Passed" >> reports/testReport.html
 else
-	echo "<p style="color:red"> Failed.</p>" >> /home/wright/testCases/reports/testReport.html 
+   echo "style=\"color:#FF0000;\">Failed" >> reports/testReport.html
 fi
-echo "</td>" >> /home/wright/testCases/reports/testReport.html
+echo "</td>" >> reports/testReport.html
 
 
 
 
-echo "</tr>" >> /home/wright/testCases/reports/testReport.html #ends a row in the table
-echo "test case $testNum complete"
-((testNum++))
+echo "</tr>" >> reports/testReport.html #ends a row in the table
+echo "test case $testCaseTracker complete"
+((testCaseTracker++))
 cd testCases
 done
 
-cd /home/wright/testCases/reports
+cd ../reports #goes to report dir
 
-xdg-open testReport.html
-
-
+xdg-open testReport.html #opens the html file
