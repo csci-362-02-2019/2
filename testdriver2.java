65c5cde36f34aed96b500bfcbeef5776206035c2
Sam Lemon <lemonsa1@g.cofc.edu>
2019-11-10 18:31:06 -0500

    driver/script progress

--- testdriver2.java
+++ testdriver2.java
@@ -3,6 +3,7 @@ import java.nio.file.Paths;
 import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.util.Random;
 
 class testdriver2 {
 	
@@ -30,24 +31,58 @@ class testdriver2 {
 		return false;
 	}
 	
+
 	public static void main(String[] args) {
 		try {
-			//System.out.println("HelloWorld! this is new");
+			//testcase 1
 			testdriver2 test1 = new testdriver2();
-			String[] array = {"Sam","Wright"};
-			String sam = "Sam";
 			String line1 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase1")).get(4);
-			String[] array1 = line1.split("|");
-			System.out.println(test1.isStringInArray(array1[0], array1));
+			String[] array1 = line1.split("&",2);
+			String[] Xarray1 = array1[1].split(",");
 
-			String results = Boolean.toString(test1.isStringInArray(array1[0], array1));
+			//testcase 2
+			String line2 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase2")).get(4);
+			String[] array2 = line2.split("&",2);
+			String[] Xarray2 = array2[1].split(",");
 
-			BufferedWriter writer = new BufferedWriter(new FileWriter("/home/samlemon/2/testcase1Report.txt"));
-    			writer.write(results);
-     
+			//testcase 3
+			String line3 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase3")).get(4);
+			String[] array3 = line3.split("&",2);
+			String[] Xarray3 = array3[1].split(",");
+
+			//testcase 4
+			String line4 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase4")).get(4);
+			
+			//testcase 5
+			String line5 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase5")).get(4);
+
+			//testcase 6
+			String line6 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase6")).get(4);
+
+			//saving results of testcases
+			String result1 = Boolean.toString(test1.isStringInArray(array1[0], Xarray1));
+			String result2 = Boolean.toString(test1.isStringInArray(array2[0], Xarray2));
+			String result3 = Boolean.toString(test1.isStringInArray(array3[0], Xarray3));
+			String result4 = Boolean.toString(test1.containsDigit(line4));
+			String result5 = Boolean.toString(test1.containsDigit(line5));
+			String result6 = Boolean.toString(test1.containsDigit(line6));
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter("../2/testcase1Report.txt"));
+    			writer.write(result1);
+			writer.newLine();
+    			writer.write(result2);
+			writer.newLine();
+    			writer.write(result3);
+			writer.newLine();
+    			writer.write(result4);
+			writer.newLine();
+    			writer.write(result5);
+     			writer.newLine();
+    			writer.write(result6);
+
     			writer.close();
 
-			//System.out.println(test1.containsDigit(sam));
+
 		} catch (IOException e) {
 			System.out.println(e);
 		}
