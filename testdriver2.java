5831cf06aa36f9d27c3c7f275a44363eb652148e
Sam Lemon <lemonsa1@g.cofc.edu>
2019-11-10 21:58:01 -0500

    finally fixed the test cases that were giving us hell HOORAH! and now we have 11 test case working

--- testdriver2.java
+++ testdriver2.java
@@ -3,7 +3,10 @@ import java.nio.file.Paths;
 import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.util.Random;
+import java.io.StringWriter;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class testdriver2 {
 	
@@ -30,34 +33,66 @@ class testdriver2 {
 		}
 		return false;
 	}
+
+	public static Integer convertToInteger(Long longValue) {
+		if (longValue < Integer.MIN_VALUE || longValue > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException(longValue + " cannot be cast to Integer without changing its value.");
+		}
+		return longValue.intValue();
+	}
 	
+	public static boolean containsUpperAndLowerCase(String test) {
+		if (test != null) {
+			Pattern pattern = Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])[\\w|\\W]*$");
+			Matcher matcher = pattern.matcher(test);
+			return matcher.matches();
+		}
+		return false;
+	}
 
 	public static void main(String[] args) {
 		try {
 			//testcase 1
 			testdriver2 test1 = new testdriver2();
-			String line1 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase1")).get(4);
+			String line1 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase001")).get(4);
 			String[] array1 = line1.split("&",2);
 			String[] Xarray1 = array1[1].split(",");
 
 			//testcase 2
-			String line2 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase2")).get(4);
+			String line2 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase002")).get(4);
 			String[] array2 = line2.split("&",2);
 			String[] Xarray2 = array2[1].split(",");
 
 			//testcase 3
-			String line3 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase3")).get(4);
+			String line3 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase003")).get(4);
 			String[] array3 = line3.split("&",2);
 			String[] Xarray3 = array3[1].split(",");
 
 			//testcase 4
-			String line4 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase4")).get(4);
+			String line4 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase004")).get(4);
 			
 			//testcase 5
-			String line5 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase5")).get(4);
+			String line5 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase005")).get(4);
 
 			//testcase 6
-			String line6 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase6")).get(4);
+			String line6 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase006")).get(4);
+
+			//testcase 7
+			String temp7 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase007")).get(4);
+			long line7 = Long.parseLong(temp7, 10);
+			
+			//testcase 8
+			String temp8 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase008")).get(4);
+			long line8 = Long.parseLong(temp8, 10);
+
+			//testcase 9
+			String line9 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase009")).get(4);
+
+			//testcase 10
+			String line10 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase010")).get(4);
+
+			//testcase 11
+			String line11 = Files.readAllLines(Paths.get("../2/TestAutomation/testCases/testcase011")).get(4);
 
 			//saving results of testcases
 			String result1 = Boolean.toString(test1.isStringInArray(array1[0], Xarray1));
@@ -66,10 +101,31 @@ class testdriver2 {
 			String result4 = Boolean.toString(test1.containsDigit(line4));
 			String result5 = Boolean.toString(test1.containsDigit(line5));
 			String result6 = Boolean.toString(test1.containsDigit(line6));
+			String result7 = Integer.toString(test1.convertToInteger(line7));
+			String result8;
+			try {
+				result8 = Integer.toString(test1.convertToInteger(line8));
+			} catch (Exception e) {
+				StringWriter sw = new StringWriter();
+				e.printStackTrace(new PrintWriter(sw));
+				String exceptionAsString = sw.toString();
+				result8 = exceptionAsString.substring(0, Math.min(exceptionAsString.length(), 100));
+			}
+			String result9 = Boolean.toString(test1.containsUpperAndLowerCase(line9));
+			String result10 = Boolean.toString(test1.containsUpperAndLowerCase(line10));
+			String result11 = Boolean.toString(test1.containsUpperAndLowerCase(line11));
+
+
+
+			//System.out.println("TC1: array1[0] = " + array1[0] + " results1 = " + result1);
+			//System.out.println("TC2: array2[0] = " + array2[0] + " results1 = " + result2);
+			//System.out.println("TC3: array3[0] = " + array3[0] + " results1 = " + result3);
 
 			//System.out.println("TC4: (" + line4 + ") output: " + result4);
+			//System.out.println("TC5: (" + line5 + ") output: " + result5);
 			//System.out.println("TC6: (" + line6 + ") output: " + result6);
 
+			//writes results to output file
 			BufferedWriter writer = new BufferedWriter(new FileWriter("../2/testcase1Report.txt"));
     			writer.write(result1);
 			writer.newLine();
@@ -82,6 +138,17 @@ class testdriver2 {
     			writer.write(result5);
      			writer.newLine();
     			writer.write(result6);
+     			writer.newLine();
+    			writer.write(result7);
+			writer.newLine();
+    			writer.write(result8);
+			writer.newLine();
+    			writer.write(result9);
+			writer.newLine();
+    			writer.write(result10);
+			writer.newLine();
+    			writer.write(result11);
+
 
     			writer.close();
 
